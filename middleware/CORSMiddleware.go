51c432ecc1901d0031afc966ccd724421ab34289
kiappk <z1652091948@outlook.com>
2023-02-12 16:06:41 +0800

    CORS Host change

--- middleware/CORSMiddleware.go
+++ middleware/CORSMiddleware.go
@@ -4,12 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// 解决跨域问题
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://172.27.224.1:3002")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", viper.GetString("cors.allowhost"))
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
